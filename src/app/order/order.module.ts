5fa572a600bcf0b60667698f8cd6c466691c5832
Omar Mohamed <omarmohamed@192.168.1.7>
2025-09-18 19:07:55 +0300

    dd

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { OrderService } from "./order.service";
 import { CartModule } from "../cart/cart.module";
 import { NotificationModule } from "../notification/notification.module";
+import { OrderController } from "./order.controller";
 
 @Module({
   imports: [CartModule, NotificationModule],
   providers: [OrderService],
+  controllers: [OrderController],
 })
 export class OrderModule {}
